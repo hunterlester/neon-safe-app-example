907abd272101d2eaff96741d79fcd7456b3a8675
hunterlester <guilfordhunter@gmail.com>
2017-10-05 16:44:29 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,115 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+let shell = require('electron').shell;
+let ipcMain = require('electron').ipcMain;
+const safeAppNeon = require('safe_app_neon');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const Menu = electron.Menu;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.webContents.on('context-menu', (e, props) => {
+        const { x, y } = props;
+
+        Menu.buildFromTemplate([{
+          label: 'Inspect element',
+          click() {
+            mainWindow.inspectElement(x, y);
+          }
+        }]).popup(mainWindow);
+      });
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  const listenForAuthReponse = (event, response) => {
+    console.log(response);
+  };
+
+  ipcMain.on('auth-response', listenForAuthReponse);
+
+  const parseUrl = (url) => (
+    (url.indexOf('safe-auth://') === -1) ? url.replace('safe-auth:', 'safe-auth://') : url
+  );
+
+  let authUri = safeAppNeon.gen_auth_uri();
+
+  shell.openExternal(parseUrl(authUri));
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+const sendResponse = (success) => {
+  mainWindow.webContents.send('auth-response', success ? success : '');
+};
+
+app.on('open-url', function (e, url) {
+  console.log('open-url event triggered');
+  sendResponse(url);
+});
+
+const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+  if (commandLine.length >= 2 && commandLine[1]) {
+        sendResponse(commandLine[1]);
+      }
+  // Someone tried to run a second instance, we should focus our window.
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+  }
+})
+
+if (shouldQuit) {
+  app.quit()
+}
